b50f3747372a5a51fe28fdd190b607fd8ee7b897
Michael <max2sax_work@outlook.com>
2020-03-08 14:22:37 +0000

    create initial react app from tutorial.

---
+++ SearchBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// class SearchBar extends React.Component {
+//     componentDidMount(){};
+//     componentDidUpdate(){};
+//     render(){
+//         // text area with magnifying glass icon:k
+//     };
+// }
+    // try with hooks:
+export default function SearchBar() {
+
+    return (
+        <div id="podcast-search-bar">
+            <input
+                type="text"/>
+            <>
+        </div>
+    );
+}
