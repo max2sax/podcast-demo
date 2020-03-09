2a0bfc25a720a0eed70f0cf7ef4b2742bc7b3384
Michael <max2sax_work@outlook.com>
2020-03-09 14:03:43 +0000

    Add data from real podcasts. Add audio player to support playing episodes.

--- src/App.js
+++ src/App.js
@@ -3,25 +3,86 @@ import logo from './logo.svg';
 import './App.css';
 import PodcastList from './PodcastList.js';
 import SearchBar from './SearchBar.js';
+import { Media, Player, controls  } from 'react-media-player'
+const { PlayPause, MuteUnmute  } = controls
+
 
 function App() {
+    // TODO: add podcast objects to state here
+    // this way we can update the podcast list from the search bar
+    // const [podcasts, setPodcasts] = React.useState([]);
+
+    const [selectedPodcastId, setSelectedPodcast] = React.useState('unique-podcast-id-1');
+    const [podcasts, setPodcasts] = React.useState(
+    [
+       {
+            ID: 'unique-podcast-id-1',
+            Name: 'Planet Money',
+            FeedURL: 'https://www.npr.org/rss/podcast.php?id=510289',
+            CoverURL: 'https://media.npr.org/assets/img/2018/08/02/npr_planetmoney_podcasttile_sq-7b7fab0b52fd72826936c3dbe51cff94889797a0.jpg?s=1400',
+            Episodes: [
+                {
+                    ID: 'unique-podcast-1-episode-id-1',
+                    Name: '#977: Where\'s The Vaccine?',
+                    MediaSource: 'https://play.podtrac.com/npr-510289/edge1.pod.npr.org/anon.npr-podcasts/podcast/npr/pmoney/2020/03/20200306_pmoney_pmpod977-9d21f76b-71be-4fcc-8fd7-f6ee5d75ad81.mp3?awCollectionId=510289&awEpisodeId=812943907&orgId=1&topicId=1031&aggIds=812054919&d=1236&p=510289&story=812943907&t=podcast&e=812943907&size=19747326&ft=pod&f=510289'
+                },
+                {
+                    ID: 'unique-podcast-1-episode-id-2',
+                    Name: 'Episode 2',
+                    MediaURL: ''
+                },
+                {
+                    ID: 'unique-podcast-1-episode-id-3',
+                    Name: 'Episode 3',
+                    MediaURL: ''
+                }
+            ]
+        },
+        {
+            ID: 'unique-podcast-id-2',
+            Name: 'Planet Money',
+            FeedURL: 'https://www.npr.org/rss/podcast.php?id=510289',
+            CoverURL: 'https://media.npr.org/assets/img/2018/08/02/npr_planetmoney_podcasttile_sq-7b7fab0b52fd72826936c3dbe51cff94889797a0.jpg?s=1400',
+            Episodes: [
+                {
+                    ID: 'unique-podcast-2-episode-id-1',
+                    Name: 'Episode 1'
+                },
+                {
+                    ID: 'unique-podcast-2-episode-id-2',
+                    Name: 'Episode 2'
+                },
+                {
+                    ID: 'unique-podcast-2-episode-id-3',
+                    Name: 'Episode 3'
+                }
+            ]
+         }
+    ]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <SearchBar />
-        <PodcastList />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <PodcastList
+          podcasts={podcasts}
+          updateSelectedPodcast={setSelectedPodcast}
+        />
+        <img
+          src={podcasts[0].CoverURL}
+          className="App-logo"
+          alt="logo"
+        />
+          <Media>
+            <div className="media">
+              <div className="media-player">
+                <Player src={podcasts[0].Episodes[0].MediaUrl} />
+              </div>
+              <div className="media-controls">
+                <PlayPause />
+                <MuteUnmute />
+              </div>
+            </div>
+          </Media>
       </header>
     </div>
   );
--- src/PodcastList.js
+++ src/PodcastList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { makeStyles  } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -7,66 +8,84 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
 import Cast from '@material-ui/icons/Cast';
+import Divider from '@material-ui/core/Divider';
 
+const useStyles = makeStyles({
+  CollapsedListStyle: {
+    paddingLeft: 4,
+  },
+  PodcastContainerStyle: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  ListItemStyle: {
+    display: 'flex',
+    flexDirection: 'row',
+  }
+});
 export default function PodcastList(props) {
-    // expects an array of podcast objects as props/state? not sure yet. For now hard code it.
-    // with new hooks paradigm.
-    const [open, setOpen] = React.useState(false);
+    // expects an array of podcast objects as props
+    const classes = useStyles();
+  const [open, setOpen] = React.useState(() => {
+    let initialState = {};
+    props.podcasts.forEach((podcast) => {
+      initialState[podcast.ID] = false;
+    });
+    return initialState;
+  });
 
-    const handleClick = () => {
-            setOpen(!open);
+    const handleClick = (e) => {
+      setOpen((prevState) => {
+        let newState = {};
+        newState[e.currentTarget.id] = !prevState[e.currentTarget.id];
+        const updatedState = {...prevState, ...newState};
+        return updatedState;
+      });
+        if (props.podcastSelected) {
+            props.podcastSelected(e.currentTarget.id);
+        }
     };
+
+    const listItems = props.podcasts.map((podcast) => {
+      const episodes = podcast.Episodes.map((episode) => {
+        return (
+        <ListItem key={episode.ID} className={classes.CollapsedListStyle}>
+          <ListItemText primary={episode.Name} />
+        </ListItem>
+        )
+      });
+      return (
+        <div
+          className={classes.PodcastContainerStyle}
+        >
+        <ListItem button
+          key={podcast.ID}
+          id={podcast.ID}
+          onClick={handleClick}
+        >
+          <div className={classes.ListItemStyle} >
+            <ListItemIcon >
+                <Cast />
+            </ListItemIcon>
+            <ListItemText primary={podcast.Name} />
+            {open[podcast.ID] ? <ExpandLess /> : <ExpandMore />}
+          </div>
+         </ListItem>
+         <Collapse in={open[podcast.ID]} timeout="auto" unmountOnExit >
+           <List component="div" disablePadding>
+             {episodes}
+           </List>
+         </Collapse>
+         </div>
+      )
+    });
+
     return (
         <div id="podcast-list-container">
             <List
                 id="podcast-list"
             >
-                <ListItem
-                    id='podcast1'
-                    onClick={handleClick}
-                >
-                    <ListItemIcon >
-                        <Cast />
-                    </ListItemIcon>
-                    <ListItemText primary="Podcast 1" />
-                    {open ? <ExpandLess /> : <ExpandMore />}
-                </ListItem>
-                <Collapse in={open} timeout="auto" unmountOnExit>
-                    <List component="div" disablePadding>
-                      <ListItem button >
-                        <ListItemText primary="Episode 1" />
-                      </ListItem>
-                      <ListItem button >
-                        <ListItemText primary="Episode 2" />
-                      </ListItem>
-                      <ListItem button >
-                        <ListItemText primary="Episode 3" />
-                      </ListItem>
-                    </List>
-                </Collapse>
-                <ListItem
-                    id='podcast2'
-                    onClick={handleClick}
-                >
-                    <ListItemIcon >
-                        <Cast />
-                    </ListItemIcon>
-                    <ListItemText primary="Podcast 2" />
-                    {open ? <ExpandLess /> : <ExpandMore />}
-                </ListItem>
-                <Collapse in={open} timeout="auto" unmountOnExit>
-                    <List component="div" disablePadding>
-                      <ListItem button >
-                        <ListItemText primary="Episode 1" />
-                      </ListItem>
-                      <ListItem button >
-                        <ListItemText primary="Episode 2" />
-                      </ListItem>
-                      <ListItem button >
-                        <ListItemText primary="Episode 3" />
-                      </ListItem>
-                    </List>
-                </Collapse>
+              {listItems}
             </List>
         </div>
     );
